cc118b249ba7c25a2f7f3296f30fc63056cc8076
stefano.antonelli <stefano.ant@gmail.com>
2012-08-13 08:25:28 +0200

    Initial Commit

---
+++ SASlideMenu/SASlideMenuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SASlideMenuAppDelegate.h
+//  SASlideMenu
+//
+//  Created by Stefano Antonelli on 8/13/12.
+//  Copyright (c) 2012 Stefano Antonelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SASlideMenuAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
